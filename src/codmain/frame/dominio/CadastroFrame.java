84a7bf89defc943bd90d48255b187ef79dae6eb4
Ezequiel <ezequielsilvalima01@gmail.com>
2025-07-15 23:55:29 -0300

    commit

--- src/codmain/frame/dominio/CadastroFrame.java
+++ src/codmain/frame/dominio/CadastroFrame.java
@@ -1,40 +1,43 @@
 package codmain.frame.dominio;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CadastroFrame {
-    private JFrame titulo;
+    private JFrame rotulo;
 
     public CadastroFrame() {
+        rotulo = new JFrame("Cadastro");
+        rotulo.setSize(400, 500);
+        rotulo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rotulo.setLayout(new GridBagLayout());
+
         startFrameMain();
     }
 
     public void startFrameMain(){
-        titulo = new JFrame("Cadastro");
-        titulo.setSize(400, 500);
-        titulo.setDefaultCloseOperation(titulo.EXIT_ON_CLOSE);
-        titulo.setLayout(null);
+
 
         JLabel infoUsuario = new JLabel("Escolha um nome de usuário:");
         infoUsuario.setBounds(115, 40, 180, 25);
-        titulo.add(infoUsuario);
+        rotulo.add(infoUsuario);
 
-        JTextField usuarioCampo = new JTextField();
-        usuarioCampo.setBounds(100, 70, 200, 25);
-        titulo.add(usuarioCampo);
+        JTextField campoUsuario = new JTextField();
+        campoUsuario.setBounds(100, 70, 200, 25);
+        rotulo.add(campoUsuario);
 
         JLabel infoSenha = new JLabel("Escolha uma senha:");
         infoSenha.setBounds(140, 120, 150, 25);
-        titulo.add(infoSenha);
+        rotulo.add(infoSenha);
 
         JPasswordField senhaCampo = new JPasswordField();
         senhaCampo.setBounds(100, 150, 200, 25);
-        titulo.add(senhaCampo);
+        rotulo.add(senhaCampo);
 
 
     }
 
     public void exibir(){
-        titulo.setVisible(true);
+        rotulo.setVisible(true);
     }
 }
