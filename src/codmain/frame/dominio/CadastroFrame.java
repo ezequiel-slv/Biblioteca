5baa766b7c7f9ed6f6a271971d169423933892f1
Ezequiel <ezequielsilvalima01@gmail.com>
2025-07-20 23:44:44 -0300

    commit

--- src/codmain/frame/dominio/CadastroFrame.java
+++ src/codmain/frame/dominio/CadastroFrame.java
@@ -6,35 +6,20 @@ import java.awt.*;
 public class CadastroFrame {
     private JFrame rotulo;
 
-    public CadastroFrame() {
-        rotulo = new JFrame("Cadastro");
-        rotulo.setSize(400, 500);
-        rotulo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        rotulo.setLayout(new GridBagLayout());
+    private JLabel textoUsuario;
+    private JTextField campoUsuaro;
 
+    private JLabel textoSenha;
+    private JTextField campoSenha;
+    public CadastroFrame() {
         startFrameMain();
     }
 
     public void startFrameMain(){
-
-
-        JLabel infoUsuario = new JLabel("Escolha um nome de usuário:");
-        infoUsuario.setBounds(115, 40, 180, 25);
-        rotulo.add(infoUsuario);
-
-        JTextField campoUsuario = new JTextField();
-        campoUsuario.setBounds(100, 70, 200, 25);
-        rotulo.add(campoUsuario);
-
-        JLabel infoSenha = new JLabel("Escolha uma senha:");
-        infoSenha.setBounds(140, 120, 150, 25);
-        rotulo.add(infoSenha);
-
-        JPasswordField senhaCampo = new JPasswordField();
-        senhaCampo.setBounds(100, 150, 200, 25);
-        rotulo.add(senhaCampo);
-
-
+        rotulo = new JFrame("Cadastro");
+        rotulo.setSize(400,500);
+        rotulo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rotulo.setLayout(null);
     }
 
     public void exibir(){
